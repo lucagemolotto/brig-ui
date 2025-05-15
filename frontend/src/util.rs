d49fe54d59d62bef44528fe2b7f02ce5df17e8c0
lucagemolotto <gemolotto.luca@gmail.com>
2025-05-15 15:54:33 +0200

    added confirmation popup

--- frontend/src/util.rs
+++ frontend/src/util.rs
@@ -32,3 +32,43 @@ pub fn PopUp(#[prop(into)] show_popup: RwSignal<bool>,
         </Show>
     }
 }
+
+#[component]
+pub fn ConfirmationPopup(
+    #[prop(into)] show_popup: RwSignal<bool>,
+    #[prop(into)] message: Signal<String>,
+    #[prop(into)] on_confirm: Callback<()>,
+    #[prop(into)] on_cancel: Callback<()>,
+) -> impl IntoView {
+    view! {
+        <Show
+            when=move || show_popup.get()
+            fallback=|| view! { <div></div> }
+        >
+            <div class="popup-overlay">
+                <div class="popup confirmation-popup">
+                    <div class="popup-header">
+                        <h3>"Confirmation"</h3>
+                    </div>
+                    <div class="popup-content">
+                        <p>{move || message.get()}</p>
+                    </div>
+                    <div class="popup-actions">
+                        <button
+                            on:click=move |_| on_confirm.run(())
+                            class="confirm-button"
+                        >
+                            "Confirm"
+                        </button>
+                        <button
+                            on:click=move |_| on_cancel.run(())
+                            class="cancel-button"
+                        >
+                            "Cancel"
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </Show>
+    }
+}
